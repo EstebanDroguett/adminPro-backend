5d8a16059adbea727801b1ac54d29d2b4d78d99f
unknown <estebandroguetthuerta@gmail.com>
2020-01-14 02:03:04 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+//Inicializar variables
+var app = express();
+
+//Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
+    if( err ) throw err;
+    console.log('Base de datos \x1b[32m%s\x1b[0m', 'online');
+})
+
+//Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente' 
+    });
+
+});
+
+//Escuchar peticiones
+app.listen(3000, ()=>{
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+})
