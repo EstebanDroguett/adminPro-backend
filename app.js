b5bc61806da1723c903d346e5859ca910ff4e730
unknown <estebandroguetthuerta@gmail.com>
2020-01-16 20:47:39 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //Inicializar variables
 var app = express();
+
+//Body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
@@ -12,14 +23,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>
 })
 
 //Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente' 
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, ()=>{
---
+++ routes/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente' 
+    });
+
+});
+
+module.exports = app;
